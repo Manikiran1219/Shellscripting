f227ca20763ad351ade4d36ef78aca3e188dc1cc
Mani Koppisetti <mani.koppisetti@global.ntt>
2025-05-20 18:38:39 +0530

    Variables-Args

--- 08-Specail-vars.sh
+++ 08-Specail-vars.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "All variables passed to the script: $@"
+echo "Number of variables : $#"
+echo "Script name : $0"
+echo "Current Directory : $PWD"
+echo "User running this script : $USER"
+echo "Home Directory of the user : $HOME"
+echo "PID of the Script :$$"
+sleep 10 &
+echo "PID of the last command in background : $!"
