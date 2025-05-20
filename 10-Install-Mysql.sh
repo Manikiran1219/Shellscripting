46ff89c05f96ea3af73373838891562c3e8f5dce
Mani Koppisetti <mani.koppisetti@global.ntt>
2025-05-20 20:40:47 +0530

    Variables-Args

--- 10-Install-Mysql.sh
+++ 10-Install-Mysql.sh
@@ -10,6 +10,11 @@ else
 echo "your are running with root access"
 fi
 
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+echo "MySQL is not installed ....going to install"
 dnf install mysql -y
 
 if [ $? -eq 0 ]
@@ -19,3 +24,16 @@ else
 echo "Installing MySQL is failure"
 exit 1
 fi
+else
+echo "MySQL is already installed..Nothing to do"
+fi
+
+# dnf install mysql -y
+
+# if [ $? -eq 0 ]
+#then
+#echo "Installing MySQL is success"
+#else
+#echo "Installing MySQL is failure"
+#exit 1
+# fi
