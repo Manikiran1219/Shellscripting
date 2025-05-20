7219f84745819463f308cb03ab1a6cd362602dfb
Mani Koppisetti <mani.koppisetti@global.ntt>
2025-05-20 20:23:41 +0530

    Variables-Args

--- 10-Install-Mysql.sh
+++ 10-Install-Mysql.sh
@@ -5,6 +5,15 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
 echo "Error :: Please run this script with root access"
+exit 1 #give other than 0 upto 127
 else
 echo "your are running with root access"
 fi
+
+dnf install mysql -y
+
+if [ $? -eq 0 ]
+then
+echo "Installing MySQL is success"
+else
+echo "Installing MySQL is failure"
